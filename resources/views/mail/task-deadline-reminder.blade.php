8b4cde915c1a667fd121a97cd8b130680f1e609a
cebulix98 <sszymanski098@gmail.com>
2024-11-20 01:49:00 +0100

    add deadline scedchuler

--- resources/views/mail/task-deadline-reminder.blade.php
+++ resources/views/mail/task-deadline-reminder.blade.php
@@ -3,8 +3,8 @@ Termin twojego zadania kończy się jutro <br>
 Tytuł: {{$task->title}} <br>
 Opis: {{$task->description}} <br>
 Termin: {{$task->deadline}} <br>
-Priorytet: {{$task->priority}} <br>
-Status: {{$task->status}} <br>
+Priorytet: {{ __('validation.priority.' . $task->priority) }} <br>
+Status: {{ __('validation.status.' . $task->status) }} <br>
 Thanks,<br>
 {{ config('app.name') }}
 </x-mail::message>
